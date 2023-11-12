3aec0db08a810414e7a1de7e3b1d11cee2eed619
NeilVm <neilerickson512@gmail.com>
2023-11-12 18:39:03 -0500

    registroc

--- movilsanna/src/app/registroc/registroc.page.ts
+++ movilsanna/src/app/registroc/registroc.page.ts
@@ -18,19 +18,19 @@ export class RegistrocPage implements OnInit {
       appointment_group_id: [1],
       appointment_status_id: [1],
       doctor: [1],
-      name: ["Juan"],
-      lastname: ["Rodas"],
-      middleName: ["Guzman"],
-      gender: ["M"],
+      name: ['Juan'],
+      lastname: ['Rodas'],
+      middleName: ['Guzman'],
+      gender: ['M'],
       typeDocumentId: [1],
-      documentNumber: ["70200100"],
-      phoneNumber: ["914020123"],
+      documentNumber: ['70200100'],
+      phoneNumber: ['914020123'],
       specialityId: [1],
-      specialityName: ["Cardiologia"],
-      emailDoctor: ["juan_rodas@sanna.com"],
+      specialityName: ['Cardiologia'],
+      emailDoctor: ['juan_rodas@sanna.com'],
       userIdDoc: [1],
-      passwordDoc: ["12345678"],
-      patientId: [30],
+      passwordDoc: ['12345678'],
+      patientId: [25],
       namep: ['', Validators.required],
       lastnamep: ['', Validators.required],
       middleNamep: ['', Validators.required],
@@ -38,11 +38,11 @@ export class RegistrocPage implements OnInit {
       documentNumberp: ['', Validators.required],
       phoneNumberp: ['', Validators.required],
       insurancePlanId: [1],
-      insurancename: ["BCRP"],
+      insurancename: ['BCRP'],
       email: ['', Validators.required],
       especialidad: [1],
-      especialidadname: ["Cardiologia"],
-      centroMe: ["SANNA Clínica EL GOLF"],
+      especialidadname: ['Cardiologia'],
+      centroMe: ['SANNA Clínica EL GOLF'],
       password: ['', Validators.required]
     });
   }
@@ -58,88 +58,97 @@ export class RegistrocPage implements OnInit {
     
     if (this.formularioc.valid) {
       const dataToSend = {
-      requestmodel: {
-      id: 0,
-      agreedTime: "12:15:00",
-      agreedDate: "2023-11-12T18:13:29.673Z",
-      appointment_group_id: this.formularioc.value.appointment_group_id,
-      appointment_status_id: this.formularioc.value.appointment_status_id,
-      doctorId: 1,  
-      doctor: {
-        id: 1,
-        name: this.formularioc.value.name,
-        lastname: this.formularioc.value.lastname,
-        middleName: this.formularioc.value.middleName,
-        gender: this.formularioc.value.gender,
-        typeDocumentId: this.formularioc.value.typeDocumentId,
-        typeDocument: {
-          id: this.formularioc.value.typeDocumentId, 
-          name: 0, 
+        id: 0,
+        agreedTime: "12:15:00",
+        agreedDate: "2023-11-12T18:13:29.673Z",
+        appointment_group_id: 1,
+        appointment_status_id: 1,
+        diagnosis: "string",
+        doctorId: 1,
+        doctor: {
+          id: 1,
+          name: "Juan",
+          lastname: "Rodas",
+          middleName: "Guzman",
+          gender: "M",
+          typeDocumentId: 1,
+          typeDocument: {
+            id: 1,
+            name: 0
+          },
+          documentNumber: '70200100',
+          dateOfBirth: "2023-11-12T18:13:29.673Z",
+          phoneNumber: "914020123",
+          specialityId: 1,
+          speciality: {
+            id: 1,
+            name: "Cardiologia"
+          },
+          email: "juan_rodas@sanna.com",
+          userId: 1,
+          user: {
+            id: 1,
+            username: "70200100",
+            password: "12345678"
+          }
         },
-        documentNumber: this.formularioc.value.documentNumber,
-        dateOfBirth:"2023-11-12T18:13:29.673Z",
-        phoneNumber: this.formularioc.value.phoneNumber,
-        specialityId: this.formularioc.value.specialityId,
+        patientId: 25,
+        patient: {
+          id: 25,
+          name: this.formularioc.value.namep,
+          lastname: this.formularioc.value.lastnamep,
+          middleName: this.formularioc.value.middleNamep,
+          gender: this.formularioc.value.genderp,
+          typeDocumentId: 1,
+          typeDocument: {
+            id: 1,
+            name: 0
+          },
+          documentNumber: this.formularioc.value.documentNumberp,
+          dateOfBirth: "2023-11-12T18:13:29.673Z",
+          phoneNumber: this.formularioc.value.phoneNumberp,
+          insurancePlanId: this.formularioc.value.insurancePlanId,
+          insurancePlan: {
+            id: 1,
+            name: 'BCRP'
+          },
+          email: this.formularioc.value.email,
+          comment: 'S/C',
+          userId: 0,
+          user: {
+            id: 0,
+            username: this.formularioc.value.documentNumberp,
+            password: this.formularioc.value.password
+          }
+        },
+        specialityId: 1,
         speciality: {
-          id: this.formularioc.value.specialityId, 
-          name: this.formularioc.value.specialityName,  
+          id: 1,
+          name: this.formularioc.value.especialidadname
         },
-        email: this.formularioc.value.emailDoctor,  
-        userId: this.formularioc.value.userIdDoc,  
-        user: {
-          id: this.formularioc.value.userIdDoc,  
-          username: this.formularioc.value.documentNumber,
-          password: this.formularioc.value.passwordDoc,  
-        }
-      },
-      patientId: this.formularioc.value.patientId,  
-      patient: {
-        id: this.formularioc.value.patientId,
-        name: this.formularioc.value.namep,  
-        lastname: this.formularioc.value.lastnamep,  
-        middleName: this.formularioc.value.middleNamep,  
-        gender: this.formularioc.value.genderp,  
-        typeDocumentId: this.formularioc.value.typeDocumentId,  
-        typeDocument: {
-          id: this.formularioc.value.typeDocumentId,  
-          name: 0,  
-        },
-        documentNumber: this.formularioc.value.documentNumberp,  
-        dateOfBirth: this.formularioc.value.agreedDate,
-        phoneNumber: this.formularioc.value.phoneNumberp,  
-        insurancePlanId:this.formularioc.value.insurancePlanId,  
-        insurancePlan: {
-          id: this.formularioc.value.insurancePlanId,  
-          name: 'BCRP',  
-        },
-        email: this.formularioc.value.email,  
-        comment: 'S/C',  
-        userId: 0,  
-        user: {
-          id: 0,  
-          username: this.formularioc.value.documentNumberp,  
-          password: this.formularioc.value.password,  
-        }
-      },
-      specialityId: 1,  
-      speciality: {
-        id: 1,  
-        name: this.formularioc.value.especialidadname,  
-      },
-      place: 'SANNA Clínica EL GOLF',
-      diagnosis: 'string',
+        place: 'SANNA Clínica EL GOLF',
+        
+      };
+        
     
-        }
+      const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'text/plain'
+      }),
+      responseType: 'text' as 'json'  
     };
     console.log('Datos a enviar a la API:', dataToSend);
     // Enviar la solicitud HTTP
-    this.http.post('https://ggwv2su9f5.execute-api.us-west-1.amazonaws.com/Prod/Sanna/registrarCita', dataToSend, { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) })
+    this.http.post('https://ggwv2su9f5.execute-api.us-west-1.amazonaws.com/Prod/Sanna/registrarCita', dataToSend,httpOptions,)
       .subscribe((response) => {
         console.log('Solicitud enviada con éxito', response);
         // Puedes redirigir o realizar otras acciones después de la respuesta del servidor
       }, (error) => {
         console.error('Error al enviar la solicitud', error);
-        // Manejar el error adecuadamente
+        console.error('Respuesta del servidor:', error.error); // Imprime el cuerpo de la respuesta
       });
-  } }
-}  
+      
+    } 
+  }
+}
